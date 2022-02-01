181693fedfe73f88bf627f9cd965c78c1a6fe5bc
Mohit Kumar Sharma <mohit@flux7.com>
2022-02-01 12:25:28 +0530

    Added secret passphrase

--- solution.py
+++ solution.py
@@ -8,15 +8,14 @@ def welcome_assignment_answers(question):
     answers = {
         "Are encoding and encryption the same? - Yes/No": "No",
         "Is it possible to decrypt a message without a key? - Yes/No": "No",
-        "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": "",
+        "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": "mtls",
         "Is it possible to decode a message without a key? - Yes/No": "Yes",
         "Is a hashed message supposed to be un-hashed? - Yes/No": "No",
         "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         "42b76fe51778764973077a5a94056724",
         "Is MD5 a secured hashing algorithm? - Yes/No": "No",
         "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number": 5,
-        "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number": 4,
-        "": "mtls"
+        "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number": 4
     }
     # if question == "Are encoding and encryption the same? - Yes/No":
     #     answer = "No"
