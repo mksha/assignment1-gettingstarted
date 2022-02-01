6defc06af3a04442c079d4b30cc2140cbc71daf7
Mohit Kumar Sharma <mohit@flux7.com>
2022-02-01 12:17:58 +0530

    Try the correct layer order

--- solution.py
+++ solution.py
@@ -14,7 +14,7 @@ def welcome_assignment_answers(question):
         "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         "42b76fe51778764973077a5a94056724",
         "Is MD5 a secured hashing algorithm? - Yes/No": "No",
-        "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number": "4",
+        "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number": "3",
         "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number": "3"}
     # if question == "Are encoding and encryption the same? - Yes/No":
     #     answer = "No"
